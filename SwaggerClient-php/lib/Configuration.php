4e9b7b23b042c0916367ac0c990aedd333be25b2
samyme <melaine.samy@gmail.com>
2022-01-20 12:33:39 +0100

    fix host

--- SwaggerClient-php/lib/Configuration.php
+++ SwaggerClient-php/lib/Configuration.php
@@ -80,7 +80,7 @@ class Configuration
      *
      * @var string
      */
-    protected $host = '//https://api.edenai.run/v1/';
+    protected $host = 'https://api.edenai.run/v1/';
 
     /**
      * User agent of the HTTP request, set to "PHP-Swagger" by default
